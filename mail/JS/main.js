e3f958f1a0a7832555418d8efe591464517598c1
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-12 19:46:53 +0100

    add: condizione

--- mail/JS/main.js
+++ mail/JS/main.js
@@ -1,5 +1,6 @@
 const login = document.getElementById('login')
 const inputMail = document.getElementById('input-mail')
+const text = document.getElementById('text')
 
 //lista di mail di chi può accedere
 const validMail = ['jasonsstatham@gmail.com', 'actionbronson@gmail.com', 'franklucas@gmail.com', 'peterpumpkineter@gmail.com', 'briangriffin@gmail.com',]
@@ -12,5 +13,13 @@ login.addEventListener('click', function(){
 
     console.log(mail)
 
+    //SE la lista possiede la mail dell'utente stampare "Benvenuto"
+    //ALTRIMENTI stampare "Rifiutato".
+
+    if(mail == validMail[0]){
+        text.innerHTML=('<span> Benvenuto! </span>')
+    } else {
+        text.innerHTML=('<span class="text-danger"> Rifiutato! </span>')
+    }
 
 })
