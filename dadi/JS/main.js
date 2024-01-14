303dea025a91a929dfeae85d00d01d8d987cacc5
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-14 16:57:40 +0100

    add: if pareggio

--- dadi/JS/main.js
+++ dadi/JS/main.js
@@ -6,23 +6,38 @@ const text = document.getElementById('text')
 
 //al click di start:
 
-start.addEventListener('click', function(){
-    //Generare e stampare un numero da 1 a 6 per il computer e per il giocatore
-    let numberUser = Math.floor((Math.random() * 6) + 1);
-    console.log(numberUser)
+start.addEventListener('click', function () {
 
-    userNumber.innerText = numberUser
+  //Generare e stampare un numero da 1 a 6 per il computer e per il giocatore
+  let numberUser = Math.floor((Math.random() * 6) + 1);
+  console.log(numberUser)
 
-    let numberComputer = Math.floor((Math.random() * 6) + 1);
-    console.log(numberComputer)
+  userNumber.innerText = numberUser
 
-    computerNumber.innerText = numberComputer
+  let numberComputer = Math.floor((Math.random() * 6) + 1);
+  console.log(numberComputer)
 
-    if (numberUser > numberComputer){
-      text.innerHTML=(`<h1 style="color: rgb(130, 205, 68)">HAI VINTO</h1>`)
-    } else{
-      text.innerHTML=(`<h1 style="color: red">HAI PERSO</h1>`)
-    }
+  computerNumber.innerText = numberComputer
 
-      
+  
+
+  if (numberUser > numberComputer) {
+
+    text.innerHTML = (`<h1 style="color: rgb(130, 205, 68)">HAI VINTO</h1>`)
+
+  } 
+  
+  if (numberUser == numberComputer) {
+
+    text.innerHTML = (`<h1 style="color: yellow">PAREGGIO</h1>`)
+
+  } 
+  
+  if (numberUser < numberComputer) {
+
+    text.innerHTML = (`<h1 style="color: red">HAI PERSO</h1>`)
+
+  }
+
+
 })
