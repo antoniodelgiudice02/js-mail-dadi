2583da9f7153a9acd26de517701576e3a678eef1
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-14 11:27:35 +0100

    add: math.random

--- dadi/JS/main.js
+++ dadi/JS/main.js
@@ -7,11 +7,10 @@ const computerNumber = document.getElementById('computer-number')
 
 start.addEventListener('click', function(){
     //Generare e stampare un numero da 1 a 6 per il computer e per il giocatore
-    function getRandomIntInclusive(min, max) {
-        min = Math.ceil(1);
-        max = Math.floor(6);
-        return Math.floor(Math.random() * (max - min + 1) + min); 
-        console.log(Math.random)
-      }
+    let numberUser = Math.floor((Math.random() * 6) + 1);
+    console.log(numberUser)
+
+    let numberComputer = Math.floor((Math.random() * 6) + 1);
+    console.log(numberComputer)
       
 })
